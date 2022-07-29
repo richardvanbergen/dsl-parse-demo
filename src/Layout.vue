421f96729a262fdbb1138db62e4b985ad3bae900
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-07-29 23:55:52 +0900

    fix: scrollable areas

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="bg-neutral-100 p-4 min-h-screen min-w-full lg:grid grid-cols-3 grid-rows-3 gap-4">
-    <div class="p-6 bg-white col-span-1 row-span-3 flex flex-col gap-4 drop-shadow-lg">
-      <div class="flex-1">
-        <slot name="left" />
-      </div>
+  <div class="bg-neutral-100 p-4 h-screen min-w-full lg:grid grid-cols-3 grid-rows-3 gap-4">
+    <div class="bg-white col-span-1 row-span-3 drop-shadow-lg">
+      <div class="h-full overflow-y-auto overflow-x-visible p-6 flex flex-col gap-10">
+        <div class="flex-1">
+          <slot name="left" />
+        </div>
 
-      <article class="prose">
-        <h3>Basic Info</h3>
-        <ul>
-          <li>Start by adding a new field.</li>
-          <li>Type `=` to create a new formula.</li>
-          <li>Methods like MAX() will accept parameters.</li>
-          <li>References to other fields can be achieved using the $ syntax.</li>
-        </ul>
-      </article>
+        <article class="prose">
+          <h3>Basic Info</h3>
+          <ul>
+            <li>Start by adding a new field.</li>
+            <li>Type `=` to create a new formula.</li>
+            <li>Methods like MAX() will accept parameters.</li>
+            <li>References to other fields can be achieved using the $ syntax.</li>
+            <li>Focus on a field to see the break down of the formula.</li>
+          </ul>
+
+          <h4>Examples</h4>
+
+          <pre><code>=$input.age * $node.value</code></pre>
+          <pre><code>=func_party(nest1(100), 2, 4 / 2, nest2(42, 69, 'nice'), 'string', true)</code></pre>
+        </article>
+      </div>
     </div>
 
     <div class="p-6 bg-white col-span-1 row-span-3 gap-4 drop-shadow-lg">
-      <slot name="center" />
+      <div class="h-full overflow-y-auto overflow-x-visible">
+        <slot name="center" />
+      </div>
     </div>
 
     <div class="p-6 bg-white col-span-1 row-span-1 gap-4 drop-shadow-lg">
