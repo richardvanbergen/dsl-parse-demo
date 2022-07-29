986a73d85f48d98d651cf914f1adb9e73bb5b6b5
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-07-29 23:40:22 +0900

    fix: button styling

--- src/components/Fields.vue
+++ src/components/Fields.vue
@@ -46,7 +46,7 @@ document.addEventListener("click", (e) => {
       {{ fieldName }}
     </Field>
 
-    <button type="button" @click="createEmptyField" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <button type="button" @click="createEmptyField" class="self-start inline-flex items-center px-2.5 py-1.5 border border-transparent text-md font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       Create Field
     </button>
   </div>
